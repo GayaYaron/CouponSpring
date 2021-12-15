4442c528a936d0d31275a09c8971d27a05d1d736
Gaya <yarongaya@gmail.com>
2021-12-15 18:03:54 +0200

    removed unnecessary id from error code

--- src/main/java/com/jb/GayaCouponProject/controller/model/ErrorDetail.java
+++ src/main/java/com/jb/GayaCouponProject/controller/model/ErrorDetail.java
@@ -2,13 +2,10 @@ package com.jb.GayaCouponProject.controller.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.ToString;
 
 @Data
 @AllArgsConstructor
 public class ErrorDetail {
-	@ToString.Include
-	private static final String FROM = "coupon app";
 	private int couponSystemCode;
 	private String message;
 }
