a3c5a54d42fd172e8133b461f330c5b99f1371cb
Gaya <yarongaya@gmail.com>
2021-12-23 18:43:32 +0200

    service annotation

--- src/main/java/com/jb/GayaCouponProject/service/CategoryService.java
+++ src/main/java/com/jb/GayaCouponProject/service/CategoryService.java
@@ -3,10 +3,12 @@ package com.jb.GayaCouponProject.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.jb.GayaCouponProject.model.Category;
 import com.jb.GayaCouponProject.repository.CategoryRepository;
 
+@Service
 public class CategoryService {
 
 	@Autowired
